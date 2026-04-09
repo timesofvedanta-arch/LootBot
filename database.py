4569136d47365f84850cfa51ff4a7ffdbe00f6cb
timesofvedanta-arch <times.of.vedanta@gmail.com>
2026-04-09 22:01:27 +0530

    database.py

--- database.py
+++ database.py
@@ -1,32 +1,34 @@
 from pymongo import MongoClient
 import os
 
-# MongoDB Connection (अपना लिंक यहाँ डालें)
-#MONGO_URL = "mongodb+srv://user:password@cluster.mongodb.net/myDatabase?retryWrites=true&w=majority"
+# आपका MongoDB लिंक
 MONGO_URL = "mongodb+srv://admin:Mk626425@lootcampainbot.5bzimnz.mongodb.net/?appName=Lootcampainbot"
+
 client = MongoClient(MONGO_URL)
 db = client['timesofvedanta_db']
-
-# Collections (Tables)
 offers_col = db['offers']
-users_col = db['users']
 
 def get_all_offers():
-    # यह डेटाबेस से सारे ऑफर्स लाएगा
+    # MongoDB से सारे ऑफर्स की लिस्ट लाना
     return list(offers_col.find({}))
 
 def save_offer(oid, name, status, prize, steps, terms, c_link, t_link):
-    # यह एडमिन पैनल से ऑफर सेव करेगा
+    # ऑफर को MongoDB में सेव या अपडेट करना
     offers_col.update_one(
         {"id": str(oid)},
         {"$set": {
-            "name": name, "status": status, "prize": prize,
-            "steps": steps, "terms": terms, 
-            "claim_link": c_link, "track_link": t_link
+            "id": str(oid), 
+            "name": name, 
+            "status": status, 
+            "prize": prize,
+            "steps": steps, 
+            "terms": terms, 
+            "claim_link": c_link, 
+            "track_link": t_link
         }},
         upsert=True
     )
 
 def get_offer_by_id(oid):
-    # किसी एक ऑफर की डिटेल के लिए
+    # आईडी के जरिए एक ऑफर ढूंढना
     return offers_col.find_one({"id": str(oid)})
