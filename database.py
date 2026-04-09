5fc1238d5e42284364ae4828fcdae85ab2d29987
timesofvedanta-arch <times.of.vedanta@gmail.com>
2026-04-09 20:38:32 +0530

    database.py

---
+++ database.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+import os
+
+# MongoDB Connection (अपना लिंक यहाँ डालें)
+MONGO_URL = "mongodb+srv://user:password@cluster.mongodb.net/myDatabase?retryWrites=true&w=majority"
+client = MongoClient(MONGO_URL)
+db = client['timesofvedanta_db']
+
+# Collections (Tables)
+offers_col = db['offers']
+users_col = db['users']
+
+def get_all_offers():
+    # यह डेटाबेस से सारे ऑफर्स लाएगा
+    return list(offers_col.find({}))
+
+def save_offer(oid, name, status, prize, steps, terms, c_link, t_link):
+    # यह एडमिन पैनल से ऑफर सेव करेगा
+    offers_col.update_one(
+        {"id": str(oid)},
+        {"$set": {
+            "name": name, "status": status, "prize": prize,
+            "steps": steps, "terms": terms, 
+            "claim_link": c_link, "track_link": t_link
+        }},
+        upsert=True
+    )
+
+def get_offer_by_id(oid):
+    # किसी एक ऑफर की डिटेल के लिए
+    return offers_col.find_one({"id": str(oid)})
